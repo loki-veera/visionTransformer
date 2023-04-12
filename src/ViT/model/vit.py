f7d3d60af387af8e3eae5a2755ebb2018a608b6e
loki-veera <veeramacheneni.lokesh@proton.me>
2023-04-12 11:07:37 +0200

    add learned positional encoding

--- src/ViT/model/vit.py
+++ src/ViT/model/vit.py
@@ -25,8 +25,8 @@ class ViT(nn.Module):
         super().__init__()
         self.linear_embed = nn.Linear(patch_res * patch_res, d_model)
         self.class_token = nn.Parameter(torch.rand(1, d_model))
-        self.register_buffer(
-            "pos_embed", self.__positional_encoding(n_patches**2 + 1, d_model)
+        self.pos_embed = nn.Parameter(
+            torch.randn(n_patches**2 + 1, d_model), requires_grad=True
         )
         self.encoders = nn.ModuleList(
             [ViTEncoder(d_model, n_heads) for _ in range(n_encoders)]
@@ -54,26 +54,6 @@ class ViT(nn.Module):
         output = self.final_linear(output[:, 0])
         return nn.functional.softmax(output, dim=-1)
 
-    def __positional_encoding(self, max_seq_length: int, d_model: int) -> torch.Tensor:
-        """Get the positional encoding values.
-
-        Args:
-            max_seq_length (int): Maximum sequence length.
-            d_model (int): Model dimension.
-
-        Returns:
-            torch.tensor: Positional encoding values.
-        """
-        pos_matrix = torch.zeros(max_seq_length, d_model, requires_grad=False)
-        for seq_index in range(0, max_seq_length):
-            for index in range(0, d_model // 2):
-                denom = torch.Tensor([seq_index / (10000 ** ((2 * index) / d_model))])
-                (
-                    pos_matrix[seq_index, 2 * index],
-                    pos_matrix[seq_index, 2 * index + 1],
-                ) = torch.sin(denom), torch.cos(denom)
-        return pos_matrix.unsqueeze(0)
-
 
 class ViTEncoder(nn.Module):
     def __init__(self, d_model: int, n_heads: int) -> None:
@@ -87,10 +67,10 @@ class ViTEncoder(nn.Module):
         self.attention = MultiHeadSelfAttention(d_model, n_heads)
         self.layer_norm = nn.LayerNorm(d_model)
         self.linear_layer = nn.Sequential(
-            nn.Dropout(0.5),
+            nn.Dropout(0.3),
             nn.Linear(d_model, 512),
             nn.GELU(),
-            nn.Dropout(0.5),
+            nn.Dropout(0.3),
             nn.Linear(512, d_model),
             nn.GELU(),
         )
